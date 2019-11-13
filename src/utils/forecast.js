a082e416fb6eee1d0aafb89a1222245b066cd50f
Desai M <pavan.desai-m@dxc.com>
2019-11-13 16:18:15 +0530

    Forecast change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast= (Latitude, Longitude, callback)=>{/*Number of arguments must and
         else{
             
             callback(undefined,
-                response.body.daily.data[0].summary+'It is currently '+ response.body.currently.temperature +' degree out.'+ 'There is a '+response.body.currently.precipProbability +'% chance of rain.'
+                response.body.daily.data[0].summary+'It is currently '+ response.body.currently.temperature +' degree out.'+'Todays high temperature is '+response.body.daily.data[0].temperatureHigh+' with a low of '+response.body.daily.data[0].temperatureLow+ ' There is a '+response.body.currently.precipProbability +'% chance of rain.'
             )
             
         }
